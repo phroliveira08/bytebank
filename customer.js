a7e01eba0e91b6ed18eb15cdd6e139687f39fa64
Pedro Oliveira <phr.oliveira08@gmail.com>
2021-12-07 21:27:30 -0300

    initial commit

---
+++ customer.js
@@ -0,0 +1,13 @@
+class Customer {
+  name;
+  cpf;
+  accountId;
+  balance;
+
+  constructor(name, cpf) {
+    this.name = name;
+    this.cpf = cpf;
+  }
+}
+
+export default Customer;
---
+++ index.js
@@ -0,0 +1,3 @@
+import Customer from "./customer";
+
+const customer1 = new Customer("Pedro", "123456789");
